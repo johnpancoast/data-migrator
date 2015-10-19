b0ea19de6e7788b697df7c61c53ccc0b889c7932
John Pancoast <johnpancoaster@gmail.com>
2015-10-19 14:04:53 -0700

    Added csv field to ease extracting csv field data via positions

---
+++ src/Field/CsvField.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * @package shideon/data-mover
+ * @copyright (c) 2015 John Pancoast
+ * @author John Pancoast <johnpancoaster@gmail.com>
+ * @license MIT
+ */
+
+namespace Shideon\DataMover\Field;
+
+use Shideon\DataMover\AbstractField;
+
+/**
+ * Shideon\DataMover\Field\CsvField
+ *
+ * @author John Pancoast <johnpancoaster@gmail.com>
+ */
+class CsvField extends AbstractField
+{
+    private $fieldPosition;
+
+    /**
+     * @inheritDoc
+     * @param int $fieldPosition Position of this field in an iteration of input data
+     */
+    public function __construct($name, array $constraints, $fieldPosition)
+    {
+        parent::__construct($name, $constraints);
+        $this->fieldPosition = $fieldPosition;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function extractValue($iterationInput)
+    {
+        return $iterationInput[$this->fieldPosition];
+    }
+}
