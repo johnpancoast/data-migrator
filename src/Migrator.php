1569c462aaaf59e2c722a75164588a16d221bb95
John Pancoast <shideon@gmail.com>
2015-10-22 14:45:29 -0700

    Broke Migrator->handleIteration() into smaller methods

--- src/Migrator.php
+++ src/Migrator.php
@@ -131,74 +131,28 @@ class Migrator implements MigratorInterface
         $iterationOutput = [];
 
         try {
+            // create input for this iteration
+            $iterationInput = $this->createIterationInput($iterationInput);
 
-            // attempt to set the input for this iteration, let model handle exception otherwise
-            try {
-                $iterationInput = $this->model->createIterationInput($this->iterationDefinition, $iterationInput);
-            } catch (\Exception $e) {
-                $this->model->handleIterationException($this->iterationDefinition, $e);
-            }
-
-            // collection of field exceptions for this iteration
-            $fieldViolationList = [];
-
-            // check if this iteration should still run after model createIterationInput() call above.
+            // check if this iteration should still run after createIterationInput() call above.
             if ($this->iterationDefinition->isSkippedIteration() || !$this->iterationDefinition->isContinuingIteration()) {
                 return;
             }
 
-            // attempt to begin iteration, let model handle exception otherwise
-            try {
-                $this->model->beginIteration($this->iterationDefinition, $iterationInput, $iterationOutput);
-            } catch (\Exception $e) {
-                $this->model->handleIterationException($this->iterationDefinition, $e);
-            }
+            // begin iteration
+            $this->beginIteration($iterationInput, $iterationOutput);
 
-            // check if iteration should still run after model iteration handling began
+            // check if iteration should still run after we began iteration
             if ($this->iterationDefinition->isSkippedIteration() || !$this->iterationDefinition->isContinuingIteration()) {
                 return;
             }
 
-            foreach ($this->model->getFields() as $field) {
-                $name = $field->getName();
+            // create output for this iteration
+            $iterationOutput = $this->createIterationOutput($iterationInput, $iterationOutput);
 
-                // set value of this field using extraction method.
-                // if empty use default.
-                $fieldValue = $field->extractValue($iterationInput);
-                $iterationOutput[$name] = !empty($fieldValue) ? $fieldValue : $field->getDefaultValue();
-
-                $violations = $this->validator->validateValue(
-                    $iterationOutput[$name],
-                    $field->getConstraints()
-                );
-
-                if (count($violations) > 0) {
-                    $field->handleConstraintViolations($violations);
-
-                    $fieldErrorMessages = [];
-
-                    foreach ($violations as $violation) {
-                        $fieldErrorMessages[] = $violation->getMessage();
-                    }
-
-                    $fieldViolationList[] = FieldViolationException::build(
-                        $name,
-                        implode(', ', $fieldErrorMessages)
-                    );
-                }
-            }
-
-            if (!empty($fieldViolationList)) {
-                try {
-                    $this->model->handleIterationConstraintViolations($this->iterationDefinition, $fieldViolationList);
-                } catch (\Exception $e) {
-                    $this->model->handleIterationException($this->iterationDefinition, $e);
-                }
-
-                // check if iteration should still run after model handled constraint violation
-                if ($this->iterationDefinition->isSkippedIteration() || !$this->iterationDefinition->isContinuingIteration()) {
-                    return;
-                }
+            // check if iteration should still run after creating outout
+            if ($this->iterationDefinition->isSkippedIteration() || !$this->iterationDefinition->isContinuingIteration()) {
+                return;
             }
 
         // Skippable exceptions are caught, saved, then ignored.
@@ -210,6 +164,100 @@ class Migrator implements MigratorInterface
         // must be called *after* we catch skippable model iteration exceptions above so that we can attempt
         // to end the iteration nicely in skippable situations. However, we also need this with its own catch
         // in case it *also* throws its own skippable.
+        $this->endIteration($iterationOutput);
+    }
+
+    /**
+     * Create iteration input
+     *
+     * @param $iterationInput
+     * @return mixed Iteration input
+     */
+    private function createIterationInput($iterationInput)
+    {
+        // attempt to set the input for this iteration, let model handle exception otherwise
+        try {
+            return $this->model->createIterationInput($this->iterationDefinition, $iterationInput);
+        } catch (\Exception $e) {
+            $this->model->handleIterationException($this->iterationDefinition, $e);
+        }
+    }
+
+    /**
+     * Begin iteration
+     *
+     * @param mixed $iterationInput
+     * @param mixed $iterationOutput
+     */
+    private function beginIteration(&$iterationInput, &$iterationOutput)
+    {
+        // attempt to begin iteration, let model handle exception otherwise
+        try {
+            $this->model->beginIteration($this->iterationDefinition, $iterationInput, $iterationOutput);
+        } catch (\Exception $e) {
+            $this->model->handleIterationException($this->iterationDefinition, $e);
+        }
+    }
+
+    /**
+     * Create iteration output
+     *
+     * @param $iterationInput
+     * @param $iterationOutput
+     * @return mixed Iteration output
+     */
+    private function createIterationOutput($iterationInput, $iterationOutput)
+    {
+        // collection of field exceptions for this iteration
+        $fieldViolationList = [];
+
+        foreach ($this->model->getFields() as $field) {
+            $name = $field->getName();
+
+            // set value of this field using extraction method.
+            // if empty use default.
+            $fieldValue = $field->extractValue($iterationInput);
+            $iterationOutput[$name] = !empty($fieldValue) ? $fieldValue : $field->getDefaultValue();
+
+            $violations = $this->validator->validateValue(
+                $iterationOutput[$name],
+                $field->getConstraints()
+            );
+
+            if (count($violations) > 0) {
+                $field->handleConstraintViolations($violations);
+
+                $fieldErrorMessages = [];
+
+                foreach ($violations as $violation) {
+                    $fieldErrorMessages[] = $violation->getMessage();
+                }
+
+                $fieldViolationList[] = FieldViolationException::build(
+                    $name,
+                    implode(', ', $fieldErrorMessages)
+                );
+            }
+        }
+
+        if (!empty($fieldViolationList)) {
+            try {
+                $this->model->handleIterationConstraintViolations($this->iterationDefinition, $fieldViolationList);
+            } catch (\Exception $e) {
+                $this->model->handleIterationException($this->iterationDefinition, $e);
+            }
+        }
+
+        return $iterationOutput;
+    }
+
+    /**
+     * End iteration
+     *
+     * @param $iterationOutput
+     */
+    private function endIteration($iterationOutput)
+    {
         try {
             $this->model->endIteration($this->iterationDefinition, $iterationOutput);
         } catch (SkippableModelIterationException $e) {
